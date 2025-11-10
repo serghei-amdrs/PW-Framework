eceb2ff9359c97276728cd076291fd2b982292ce
Serghei Borisenco <serghei.borisenco@amdaris.com>
2025-11-10 12:54:14 +0200

    Setup Environment Variables

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// Determine which environment file to load
+const environmentPath = process.env.ENVIRONMENT 
+    ? `./env/.env.${process.env.ENVIRONMENT}`
+    : `./env/.env.dev`; // Default to dev environment
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
+// Load the environment variables
+dotenv.config({
+    path: environmentPath,
+});
+
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
