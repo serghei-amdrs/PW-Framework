56c3566e167407e31734f9afab4f1a99c1b26b86
Serghei Borisenco <serghei.borisenco@amdaris.com>
2025-11-21 13:10:29 +0200

    Article and auth API tests

---
+++ tests/api/article.spec.ts
@@ -0,0 +1,83 @@
+import { ArticleResponseSchema } from '../../fixtures/api/schemas';
+import { ArticleResponse } from '../../fixtures/api/types-guards';
+import { test, expect } from '../../fixtures/pom/test-options';
+import articleData from '../../test-data/articleData.json';
+
+test.describe('Verify CRUD for Article', () => {
+    test('Verify Create/Read/Update/Delete an Article', { tag: '@Api' },
+        async ({ apiRequest }) => {
+            let articleId: string;
+            await test.step('Verify Create an Article', async () => {
+                const { status, body } = await apiRequest<ArticleResponse>({
+                    method: 'POST',
+                    url: 'api/articles/',
+                    baseUrl: process.env.API_URL,
+                    body: articleData.create,
+                    headers: process.env.ACCESS_TOKEN,
+                });
+                expect(status).toBe(201);
+                expect(ArticleResponseSchema.parse(body)).toBeTruthy();
+                articleId = body.article.slug;
+            });
+
+            await test.step('Verify Read an Article', async () => {
+                const { status, body } = await apiRequest<ArticleResponse>({
+                    method: 'GET',
+                    url: `api/articles/${articleId}`,
+                    baseUrl: process.env.API_URL,
+                });
+
+                expect(status).toBe(200);
+                expect(ArticleResponseSchema.parse(body)).toBeTruthy();
+            });
+
+            await test.step('Verify Update an Article', async () => {
+                const { status, body } = await apiRequest<ArticleResponse>({
+                    method: 'PUT',
+                    url: `api/articles/${articleId}`,
+                    baseUrl: process.env.API_URL,
+                    body: articleData.update,
+                    headers: process.env.ACCESS_TOKEN,
+                });
+
+                expect(status).toBe(200);
+                expect(ArticleResponseSchema.parse(body)).toBeTruthy();
+                expect(body.article.title).toBe(articleData.update.article.title);
+                articleId = body.article.slug;
+            });
+
+            await test.step('Verify Read an Article', async () => {
+                const { status, body } = await apiRequest<ArticleResponse>({
+                    method: 'GET',
+                    url: `api/articles/${articleId}`,
+                    baseUrl: process.env.API_URL,
+                });
+
+                expect(status).toBe(200);
+                expect(ArticleResponseSchema.parse(body)).toBeTruthy();
+                expect(body.article.title).toBe(articleData.update.article.title);
+            });
+
+            await test.step('Verify Delete an Article', async () => {
+                const { status, body } = await apiRequest({
+                    method: 'DELETE',
+                    url: `api/articles/${articleId}`,
+                    baseUrl: process.env.API_URL,
+                    headers: process.env.ACCESS_TOKEN,
+                });
+
+                expect(status).toBe(204);
+            });
+
+            await test.step('Verify the Article is deleted', async () => {
+                const { status, body } = await apiRequest({
+                    method: 'GET',
+                    url: `api/articles/${articleId}`,
+                    baseUrl: process.env.API_URL,
+                });
+
+                expect(status).toBe(404);
+            });
+        }
+    );
+});
---
+++ tests/api/authentification.spec.ts
@@ -0,0 +1,87 @@
+import { ErrorResponseSchema } from '../../fixtures/api/schemas';
+import { ErrorResponse } from '../../fixtures/api/types-guards';
+import { test, expect } from '../../fixtures/pom/test-options';
+import invalidCredentials from '../../test-data/invalidCredentials.json';
+
+test.describe('Verify API Validation for Log In / Sign Up', () => {
+    test('Verify API Validation for Log In', { tag: '@Api' }, async ({ apiRequest }) => {
+        const { status, body } = await apiRequest<ErrorResponse>({
+            method: 'POST',
+            url: 'api/users/login',
+            baseUrl: process.env.API_URL,
+            body: {
+                user: {
+                    email: invalidCredentials.invalidEmails[0],
+                    password: invalidCredentials.invalidPasswords[0],
+                },
+            },
+        });
+
+        expect(status).toBe(403);
+        expect(ErrorResponseSchema.parse(body)).toBeTruthy();
+    }
+    );
+
+    test('Verify API Validation for Sign Up', { tag: '@Api' }, async ({ apiRequest }) => {
+        await test.step('Verify API Validation for Invalid Email', async () => {
+            for (const invalidEmail of invalidCredentials.invalidEmails) {
+                const { status, body } = await apiRequest<ErrorResponse>({
+                    method: 'POST',
+                    url: 'api/users',
+                    baseUrl: process.env.API_URL,
+                    body: {
+                        user: {
+                            email: invalidEmail,
+                            password: '8charact',
+                            username: 'testuser',
+                        },
+                    },
+                });
+
+                expect(status).toBe(422);
+                expect(ErrorResponseSchema.parse(body)).toBeTruthy();
+            }
+        });
+
+        await test.step('Verify API Validation for Invalid Password', async () => {
+            for (const invalidPassword of invalidCredentials.invalidPasswords) {
+                const { status, body } = await apiRequest<ErrorResponse>({
+                    method: 'POST',
+                    url: 'api/users',
+                    baseUrl: process.env.API_URL,
+                    body: {
+                        user: {
+                            email: 'validEmail@test.com',
+                            password: invalidPassword,
+                            username: 'testuser',
+                        },
+                    },
+                });
+
+                expect(status).toBe(422);
+                expect(ErrorResponseSchema.parse(body)).toBeTruthy();
+            }
+        });
+
+        await test.step('Verify API Validation for Invalid Email', async () => {
+            for (const invalidUsername of invalidCredentials.invalidUsernames) {
+                const { status, body } = await apiRequest<ErrorResponse>({
+                    method: 'POST',
+                    url: 'api/users',
+                    baseUrl: process.env.API_URL,
+                    body: {
+                        user: {
+                            email: 'validEmail@test.com',
+                            password: '8charact',
+                            username: invalidUsername,
+                        },
+                    },
+                });
+
+                expect(status).toBe(422);
+                expect(ErrorResponseSchema.parse(body)).toBeTruthy();
+            }
+        });
+    }
+    );
+});
